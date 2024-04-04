2e074dc07c1d6fb595712a89ac63689d12cd84ad
ngnthien18 <nguyenngocanhthien73@gmail.com>
2024-04-04 16:53:15 +0700

    complete upload

---
+++ KhachSan/KhachSan/Controllers/HomeController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
+using KhachSan.Models.DB;
+
+namespace HotelManagmentSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult sendmail()
+        {
+           
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult sendmail(KhachSan.Models.MailModel _objModelMail)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    MailMessage mail = new MailMessage();
+                    mail.To.Add(_objModelMail.To);
+                    //mail.From = new MailAddress(_objModelMail.From);
+                    mail.From = new MailAddress("hotelservcies@gmail.com", "hotel");
+                    mail.Subject = _objModelMail.Subject;
+                    string Body = _objModelMail.Body;
+                    mail.Body = Body;
+                    mail.IsBodyHtml = true;
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.UseDefaultCredentials = false;
+
+                    smtp.Credentials = new NetworkCredential("hotelservcies@gmail.com", "hotelservcies007");
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+
+                    return View("sendmail");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+
+                return View("sendmail");
+            }
+          
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(tbl_user objuser)
+        {
+            KhachSanEntities db = new KhachSanEntities();
+
+            var user = db.tbl_user.Where(x => x.email == objuser.email && x.user_password == objuser.user_password).FirstOrDefault();
+            ViewBag.u = user;
+            try
+            {
+            ViewBag.email = user.email;
+            ViewBag.password = user.user_password;
+                Session["Name"] = user.username;
+            }
+            catch (Exception)
+            {
+
+                
+            }
+            if (user != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult LoginCus()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginCus(tbl_customer objcus)
+        {
+            KhachSanEntities db = new KhachSanEntities();
+
+            var cus = db.tbl_customer.Where(x => x.emailcus == objcus.emailcus && x.cus_password == objcus.cus_password).FirstOrDefault();
+            ViewBag.u = cus;
+            try
+            {
+                ViewBag.email = cus.emailcus;
+                ViewBag.password = cus.cus_password;
+                Session["Id"] = cus.cus_id;
+                Session["CusName"] = cus.cusname;
+            }
+            catch (Exception)
+            {
+
+
+            }
+            if (cus != null)
+            {
+                return RedirectToAction("Home", "View");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(tbl_customer cus, string rePass)
+        {
+            KhachSanEntities db = new KhachSanEntities();
+            if (ModelState.IsValid)
+            {
+                var checkEmail = db.tbl_customer.FirstOrDefault(c => c.emailcus == cus.emailcus);
+                if (checkEmail != null)
+                {
+                    ViewBag.ThongBaoEmail = "Đã có tài khoản đăng nhập bằng Email này";
+                    return View();
+                }
+                if (cus.cus_password == rePass)
+                {
+                    db.tbl_customer.Add(cus);
+                    db.SaveChanges();
+                    return RedirectToAction("Home", "View");
+                }
+                else
+                {
+                    ViewBag.ThongBao = "Mật khẩu xác nhận không chính xác";
+                    return View();
+                }
+            }
+            return View();
+        }
+        public ActionResult LogOut()
+        {
+            Session["CusName"] = null;
+            Session["Id"] = null;
+
+            return RedirectToAction("Home", "View");
+        }
+
+    }
+}
---
+++ KhachSan/KhachSan/Controllers/tbl_customerController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KhachSan.Models.DB;
+
+namespace KhachSan.Controllers
+{
+    public class tbl_customerController : Controller
+    {
+        private KhachSanEntities db = new KhachSanEntities();
+
+        // GET: tbl_customer
+        public ActionResult Index()
+        {
+            return View(db.tbl_customer.ToList());
+        }
+
+        // GET: tbl_customer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_customer tbl_customer = db.tbl_customer.Find(id);
+            if (tbl_customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_customer);
+        }
+
+        // GET: tbl_customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tbl_customer/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "cus_id,cusname,emailcus,cus_password")] tbl_customer tbl_customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbl_customer.Add(tbl_customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tbl_customer);
+        }
+
+        // GET: tbl_customer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_customer tbl_customer = db.tbl_customer.Find(id);
+            if (tbl_customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_customer);
+        }
+
+        // POST: tbl_customer/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "cus_id,cusname,emailcus,cus_password")] tbl_customer tbl_customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbl_customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tbl_customer);
+        }
+
+        // GET: tbl_customer/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_customer tbl_customer = db.tbl_customer.Find(id);
+            if (tbl_customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_customer);
+        }
+
+        // POST: tbl_customer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbl_customer tbl_customer = db.tbl_customer.Find(id);
+            db.tbl_customer.Remove(tbl_customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
